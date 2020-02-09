4f4ead07a38af81564958b2fb3a0a29223d19104
carlosperate <carlosperate@users.noreply.github.com>
2020-02-09 21:01:17 +0000

    Update to the new protocol, smaller & easier to read.

--- pc_control.py
+++ pc_control.py
@@ -5,34 +5,34 @@ from microbit import uart
 class Keyboard():
 
     def write(self, text):
-        uart.write("$%@s-k-txt@%${}$%@end@%$".format(text))
+        uart.write("|@s-k-txt@|{}|@e@|".format(text))
 
 
 class Mouse():
 
     def move_up(self, pixels=1):
-        uart.write("$%@s-m-mu@%${}$%@end@%$\n".format(pixels))
+        uart.write("|@s-m-mu@|{}|@e@|\n".format(pixels))
 
     def move_down(self, pixels=1):
-        uart.write("$%@s-m-md@%${}$%@end@%$\n".format(pixels))
+        uart.write("|@s-m-md@|{}|@e@|\n".format(pixels))
 
     def move_left(self, pixels=1):
-        uart.write("$%@s-m-ml@%${}$%@end@%$\n".format(pixels))
+        uart.write("|@s-m-ml@|{}|@e@|\n".format(pixels))
 
     def move_right(self, pixels=1):
-        uart.write("$%@s-m-mr@%${}$%@end@%$\n".format(pixels))
+        uart.write("|@s-m-mr@|{}|@e@|\n".format(pixels))
 
     def move_vertical(self, pixels=1):
-        uart.write("$%@s-m-mv@%${}$%@end@%$\n".format(pixels))
+        uart.write("|@s-m-mv@|{}|@e@|\n".format(pixels))
 
     def move_horizontal(self, pixels=1):
-        uart.write("$%@s-m-mh@%${}$%@end@%$\n".format(pixels))
+        uart.write("|@s-m-mh@|{}|@e@|\n".format(pixels))
 
     def left_click(self):
-        uart.write("$%@s-m-ckl@%$$%@end@%$\n")
+        uart.write("|@s-m-ckl@||@e@|\n")
 
     def right_click(self):
-        uart.write("$%@s-m-ckr@%$$%@end@%$\n")
+        uart.write("|@s-m-ckr@||@e@|\n")
 
     def scroll(self, steps=1):
-        uart.write("$%@s-m-s@%${}$%@end@%$\n".format(steps))
+        uart.write("|@s-m-s@|{}|@e@|\n".format(steps))

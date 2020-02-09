7dabd0dcf987c1bfc47adc8192ae02e84d2e4192
carlosperate <carlosperate@users.noreply.github.com>
2020-02-09 20:38:50 +0000

    Add protocol messages for the rest of mouse methods.

--- pc_control.py
+++ pc_control.py
@@ -5,13 +5,34 @@ from microbit import uart
 class Keyboard():
 
     def write(self, text):
-        uart.write("$%@k-txt@%${}".format(text))
+        uart.write("$%@s-k-txt@%${}$%@end@%$".format(text))
 
 
 class Mouse():
 
-    def left_click():
-        uart.write("$%@m-ckl@%$")
+    def move_up(self, pixels=1):
+        uart.write("$%@s-m-mu@%${}$%@end@%$\n".format(pixels))
 
-    def right_click():
-        uart.write("$%@m-ckr@%$")
+    def move_down(self, pixels=1):
+        uart.write("$%@s-m-md@%${}$%@end@%$\n".format(pixels))
+
+    def move_left(self, pixels=1):
+        uart.write("$%@s-m-ml@%${}$%@end@%$\n".format(pixels))
+
+    def move_right(self, pixels=1):
+        uart.write("$%@s-m-mr@%${}$%@end@%$\n".format(pixels))
+
+    def move_vertical(self, pixels=1):
+        uart.write("$%@s-m-mv@%${}$%@end@%$\n".format(pixels))
+
+    def move_horizontal(self, pixels=1):
+        uart.write("$%@s-m-mh@%${}$%@end@%$\n".format(pixels))
+
+    def left_click(self):
+        uart.write("$%@s-m-ckl@%$$%@end@%$\n")
+
+    def right_click(self):
+        uart.write("$%@s-m-ckr@%$$%@end@%$\n")
+
+    def scroll(self, steps=1):
+        uart.write("$%@s-m-s@%${}$%@end@%$\n".format(steps))

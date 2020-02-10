403f2469a8005918e244b7ba2da34541a9755786
carlosperate <carlosperate@users.noreply.github.com>
2020-02-10 21:55:21 +0000

    Add example for mouse.

---
+++ examples/mouse_example.py
@@ -0,0 +1,24 @@
+from microbit import display, Image, accelerometer, button_a, button_b, sleep
+
+from pc_control import Mouse
+
+mouse = Mouse()
+
+
+def main():
+    display.show(Image.HAPPY)
+
+    while True:
+        if button_a.is_pressed():
+            x = accelerometer.get_x()
+            y = accelerometer.get_y()
+            mouse.move_relative(x // 10, y //10)
+            sleep(50)
+
+        if button_b.is_pressed():
+            mouse.left_click()
+            sleep(500)
+
+
+if __name__ == "__main__":
+    main()
